5115a136b1d6d721261d4987aaa275c05a16adc3
石橋大輝 <lspwdcd6122@gmail.com>
2020-04-01 17:47:29 +0900

    PORT再度修正

--- server/index.js
+++ server/index.js
@@ -42,7 +42,7 @@ if(process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || '3001'
 
-app.listen(PerformanceResourceTiming, function(){
+app.listen(PORT, function(){
   console.log('I am running!')
 })
 
